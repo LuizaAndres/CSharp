84fa721673f3d1885dd49ded245ca55a11286cf4
LUIZA DE PAULA ANDRES <luiza.andres@alunos.sc.senac.br>
2020-02-10 22:25:21 -0300

    aula 1

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace CSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double FATOR = 5.0/9.0;
+        double AJUSTE = 32;
+
+        double fahrenheit = 86;
+        double celsius = (fahrenheit - AJUSTE) * FATOR;
+
+        Console.WriteLine("O valor de "+ fahrenheit +" fahrenheit em Celsius é " + celsius + "°C.");
+        
+        fahrenheit = 150;
+        celsius = (fahrenheit - AJUSTE) * FATOR;
+        Console.WriteLine("O valor de "+ fahrenheit +" fahrenheit em Celsius é " + celsius + "°C.");
+        
+        }
+    }
+}
